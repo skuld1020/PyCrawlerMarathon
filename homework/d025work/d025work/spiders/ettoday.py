81c87db3b5f25d50e3b55cd536aee48dba950ed4
skuld1020 <66210904+skuld1020@users.noreply.github.com>
2020-11-04 14:01:01 +0800

    Update ettoday.py

--- homework/d025work/d025work/spiders/ettoday.py
+++ homework/d025work/d025work/spiders/ettoday.py
@@ -1,10 +1,13 @@
 import scrapy
+from d025work.items import D025WorkItem
+
 
 
 class EttodaySpider(scrapy.Spider):
     name = 'ettoday'
     allowed_domains = ['www.ettoday.net']
-    start_urls = ['https://www.ettoday.net/news/20201004/1824032.htm']
+    start_urls = ['https://www.ettoday.net/news/20201004/1824032.htm',
+                  'https://www.ettoday.net/news/20201104/1846312.htm']
 
     def parse(self, response):
         if response.status !=200:
@@ -12,9 +15,9 @@ class EttodaySpider(scrapy.Spider):
             print('{}'.format(response.url))
             return
         
-        title = response.xpath('//title/text()').get()
-        content = response.xpath('//article//div[@class="story"]/p/text()').getall()
-        print(title)
-        print(content)
+        item = D025WorkItem()
+        item['title'] = response.xpath('//title/text()').get()
+        item['content'] = response.xpath('//article//div[@class="story"]/p/text()').getall()
+        yield item
         
         
